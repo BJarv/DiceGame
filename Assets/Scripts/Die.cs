ad291b3d2475e307b0d8990aba1553f812a9f520
BJarv <brandon.jarv@gmail.com>
2015-03-28 19:43:54 -0700

    initial tests of dice hands

---
+++ Assets/Scripts/Die.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour {
+
+	public Sprite[] dieFaces;
+	public int value = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int roll() {
+		value = Random.Range (1, 7);
+		Debug.Log (value);
+		GetComponent<SpriteRenderer>().sprite = dieFaces[value - 1];
+		return value;
+	}
+}
---
+++ Assets/Scripts/circLinkListTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class circLinkListTest : MonoBehaviour {
+
+
+	string[] words = {"can", "this", "even", "be", "so", "easy?"};
+	LinkedList<string> sentence;
+	// Use this for initialization
+	void Start () {
+		sentence = new LinkedList<string>(words);
+		LinkedListNode<string> beg = sentence.First;
+		/*int i = 0;
+		for(;i < 100; beg = beg.Next ?? beg.List.First){
+			Debug.Log (beg.Value);
+			i++;
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
